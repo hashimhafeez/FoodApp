25e808e324402d3494912e61fdcfbe16442ce722
Hashim Hafeez <hashimhafeez21@gmail.com>
2023-01-05 12:30:04 +0500

    Adding on click functionality on cart

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,14 +1,14 @@
-import { Fragment } from "react";
+import React, { Fragment } from "react";
 import mealImage from "../../assets/meals.jpg";
 import classes from "./Header.module.css";
 import HeaderCartButton from "./HeaderCartButton";
 
-function Header() {
+function Header(props) {
   return (
     <Fragment>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <HeaderCartButton />
+        <HeaderCartButton onShowCart={props.onShowCart} />
       </header>
       <div className={classes["main-image"]}>
         <img src={mealImage} alt="A table full of delicious food!" />
--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,23 @@
 import React from "react";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
+import { useContext } from "react";
+import CartContext from "../../store/cart-context";
 
 function HeaderCartButton(props) {
+  const cartCntx = useContext(CartContext);
+  console.log("cart", cartCntx);
+  const numberOfCartItem = cartCntx.items.reduce((curr, item) => {
+    return curr + item.amount;
+  }, 0);
+  console.log(numberOfCartItem);
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={props.onShowCart}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfCartItem}</span>
     </button>
   );
 }
