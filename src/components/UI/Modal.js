6d5d5ac761b689e47c41b276e276ddfc3ed63e48
Hashim Hafeez <hashimhafeez21@gmail.com>
2023-01-05 12:33:15 +0500

    Adding modal window for cart item using Portals

---
+++ src/components/UI/Modal.js
@@ -0,0 +1,34 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Fragment } from "react";
+import classes from "./Modal.module.css";
+
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} onClick={props.onClick}></div>;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
+
+function Modal(props) {
+  const portalElement = document.getElementById("overlays");
+  return (
+    <Fragment>
+      {ReactDOM.createPortal(
+        <Backdrop onClick={props.onClickBackdrop} />,
+        portalElement
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        portalElement
+      )}
+    </Fragment>
+  );
+}
+
+export default Modal;
