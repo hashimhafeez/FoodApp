eed8ef0fda13b9275e2bb37531cc1852cccb0e6b
Hashim Hafeez <hashimhafeez21@gmail.com>
2023-01-05 12:28:39 +0500

    Remove static data feeding to cartItem with dynamic & and backdrop

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,28 +1,45 @@
 import React from "react";
 import classes from "./Cart.module.css";
 import Modal from "../UI/Modal";
+import { useContext } from "react";
+import CartContext from "../../store/cart-context";
+import CartItem from "./CartItem";
 
 function Cart(props) {
+  const data = React.useContext(CartContext);
+  const totalAmount = `$${data.totalAmount.toFixed(2)}`;
+  const hasItems = data.items.length > 0;
+
+  const cartItemRemoveHandler = (id) => {};
+
+  const cartItemAddHandler = (item) => {};
+
   const CartItems = (
-    <ul>
-      {[{ id: "ci", name: "Sushi", amount: "2", price: 12.99 }].map((item) => (
-        <li key={Math.random()}>{item.id}</li>
+    <ul className={classes["cart-items"]}>
+      {data.items.map((item) => (
+        <CartItem
+          key={item.id}
+          name={item.name}
+          price={item.price}
+          amount={item.price}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
+        />
       ))}
     </ul>
   );
   return (
-    <Modal>
-      CartItem
-      <div>{CartItems}</div>
+    <Modal onClickBackdrop={props.onCloseCart}>
+      {CartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>35.62</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
         <button className={classes["button--alt"]} onClick={props.onCloseCart}>
           Close
         </button>
-        <button className={classes.button}>Order</button>
+        {hasItems && <button className={classes.button}>Order</button>}
       </div>
     </Modal>
   );
